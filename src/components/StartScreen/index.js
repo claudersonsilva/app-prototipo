8596859ac7b072074218a36e5985bef03d8099d6
claudersonsilva <clauderson.silva@hotmail.com>
2022-07-27 13:07:18 -0300

    iniciando tela de inicio

---
+++ src/components/StartScreen/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+import {Container, ButtonPrimary, Title1} from './styles';
+
+export default class StartScreen extends Component{
+    render(){
+        return(
+            <Container>
+                <Title1>TESTE</Title1>
+            </Container>
+        );
+    }
+}
---
+++ src/components/StartScreen/styles.js
@@ -0,0 +1,12 @@
+import styled from "styled-components/native";
+
+
+export const Container = styled.View`
+    flex: auto;
+    justify-content: center;
+    align-itens: center;
+    background-color: #e3e3e3;
+    padding: 20px;
+`
+export const Title1 = styled.Text``
+export const ButtonPrimary = styled.TouchableOpacity``
