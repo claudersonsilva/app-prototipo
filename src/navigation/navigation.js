2a359c8e84da3b4a7612ecc9f165ac5fd46345cb
claudersonsilva <clauderson.silva@hotmail.com>
2022-07-28 17:33:43 -0300

    navegação stack meus dados

--- src/navigation/navigation.js
+++ src/navigation/navigation.js
@@ -1,18 +1,107 @@
 import React from "react";
 
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { NavigationContainer } from '@react-navigation/native';
 
 import Ionicons from '@expo/vector-icons/Ionicons';
 
 //Screens
 import Home from './../screens/home';
-import DeliveryAddress from './../screens/delivery-address';
-import Account from './../screens/account';
-import OrderHistory from './../screens/order-history';
-import Contact from './../screens/contact';
+import DeliveryAddressScreen from './../screens/delivery-address';
+import AccountScreen from './../screens/account';
+import UpdateProfileScreen from './../screens/update-profile';
+import OrderHistoryScreen from './../screens/order-history';
+import PrivatePolicyScreen from './../screens/privacy-policy';
+import HelpScreen from './../screens/help';
+import ContactScreen from './../screens/contact';
 
 const Tab = createBottomTabNavigator();
+const AccountStackNavigator = createNativeStackNavigator();
+
+function AccountStack(){
+    return(
+        <AccountStackNavigator.Navigator
+            initialRouteName="AccountScreen"
+        >
+            <AccountStackNavigator.Screen 
+                name="AccountScreen"
+                component={AccountScreen}
+                options={{
+                    headerTitle: 'MEUS DADOS',
+                    headerTitleAlign: 'center',
+                    headerStyle: {
+                        backgroundColor: '#F05742',
+                    },
+                    headerTintColor: '#fff',
+                    headerTitleStyle: {
+                        fontWeight: 'bold',
+                    }
+                }}
+            />
+            <AccountStackNavigator.Screen 
+                name="OrderHistoryScreen"
+                component={OrderHistoryScreen}
+                options={{
+                    headerTitle: 'HISTÓRICO DE PEDIDOS',
+                    headerTitleAlign: 'center',
+                    headerStyle: {
+                        backgroundColor: '#F05742',
+                    },
+                    headerTintColor: '#fff',
+                    headerTitleStyle: {
+                        fontWeight: 'bold',
+                    }
+                }}
+            />
+            <AccountStackNavigator.Screen 
+                name="PrivatePolicyScreen"
+                component={PrivatePolicyScreen}
+                options={{
+                    headerTitle: 'POLÍTICA DE PRIVACIDADE',
+                    headerTitleAlign: 'center',
+                    headerStyle: {
+                        backgroundColor: '#F05742',
+                    },
+                    headerTintColor: '#fff',
+                    headerTitleStyle: {
+                        fontWeight: 'bold',
+                    }
+                }}
+            />
+            <AccountStackNavigator.Screen 
+                name="HelpScreen"
+                component={HelpScreen}
+                options={{
+                    headerTitle: 'FAQ',
+                    headerTitleAlign: 'center',
+                    headerStyle: {
+                        backgroundColor: '#F05742',
+                    },
+                    headerTintColor: '#fff',
+                    headerTitleStyle: {
+                        fontWeight: 'bold',
+                    }
+                }}
+            />
+            <AccountStackNavigator.Screen 
+                name="UpdateProfileScreen"
+                component={UpdateProfileScreen}
+                options={{
+                    headerTitle: 'ATUALIZAR PERFIL',
+                    headerTitleAlign: 'center',
+                    headerStyle: {
+                        backgroundColor: '#F05742',
+                    },
+                    headerTintColor: '#fff',
+                    headerTitleStyle: {
+                        fontWeight: 'bold',
+                    }
+                }}
+            />
+        </AccountStackNavigator.Navigator>
+    );
+}
 
 function MyTabs() {
     return (
@@ -40,12 +129,13 @@ function MyTabs() {
             </Tab.Screen>
             <Tab.Screen
                 name="Account"
-                component={Account}
+                component={AccountStack}
                 options={{
+                    headerShown: false,
                     headerTitle: 'MEUS DADOS',
                     headerTitleAlign: 'center',
                     headerStyle: {
-                        backgroundColor: '#F05742',
+                        backgroundColor: '#fff',
                     },
                     headerTintColor: '#fff',
                     headerTitleStyle: {
@@ -62,8 +152,8 @@ function MyTabs() {
                 }}
             ></Tab.Screen>
             <Tab.Screen
-                name="Contact"
-                component={Contact}
+                name="ContactScreen"
+                component={ContactScreen}
                 options={{
                     headerTitle: 'FALE CONOSCO',
                     headerTitleAlign: 'center',
--- src/screens/account/index.js
+++ src/screens/account/index.js
@@ -1,36 +1,40 @@
 import React from 'react';
+import { useNavigation } from '@react-navigation/native';
 
 import Ionicons from '@expo/vector-icons/Ionicons';
 
-import { Container, ButtonAtualizarDados, DivAtualizarDados, NomeClienteText, Text, DivMenuList, ItemButtonList, ItemMenuText} from '../account/styles';
+import { Container, ButtonAtualizarDados, DivAtualizarDados, NomeClienteText, Text, DivMenuList, ItemButtonList, ItemMenuText } from '../account/styles';
 import colors from './../../global-styles/colors';
 
+
 export default () => {
+    const navigation = useNavigation();
     return (
         <Container>
-            <ButtonAtualizarDados>
+            <ButtonAtualizarDados
+                onPress={() => navigation.navigate('UpdateProfileScreen')}>
                 <Ionicons name="md-person-circle-outline" size={48} color="gray" />
                 <DivAtualizarDados>
                     <NomeClienteText>Olá, Fulano da Silva Sauro</NomeClienteText>
                     <Text>ATUALIZAR MEUS DADOS</Text>
                 </DivAtualizarDados>
             </ButtonAtualizarDados>
-
-            
-
             <DivMenuList>
-                    <ItemButtonList>
-                        <Ionicons name="receipt-outline" size={32} color={colors.primary} />
-                        <ItemMenuText>MEU HISTÓRICO DE PEDIDOS</ItemMenuText>
-                    </ItemButtonList>
-                    <ItemButtonList>
-                        <Ionicons name="shield-outline" size={32} color={colors.primary} />
-                        <ItemMenuText>POLÍTICA DE PRIVACIDADE</ItemMenuText>
-                    </ItemButtonList>
-                    <ItemButtonList>
-                        <Ionicons name="md-help-circle-outline" size={32} color={colors.primary} /> 
-                        <ItemMenuText>AJUDA</ItemMenuText>
-                    </ItemButtonList>
+                <ItemButtonList
+                    onPress={() => navigation.navigate('OrderHistoryScreen')}>
+                    <Ionicons name="receipt-outline" size={32} color={colors.primary} />
+                    <ItemMenuText>MEU HISTÓRICO DE PEDIDOS</ItemMenuText>
+                </ItemButtonList>
+                <ItemButtonList
+                    onPress={() => navigation.navigate('PrivatePolicyScreen')}>
+                    <Ionicons name="shield-outline" size={32} color={colors.primary} />
+                    <ItemMenuText>POLÍTICA DE PRIVACIDADE</ItemMenuText>
+                </ItemButtonList>
+                <ItemButtonList
+                    onPress={() => navigation.navigate('HelpScreen')}>
+                    <Ionicons name="md-help-circle-outline" size={32} color={colors.primary} />
+                    <ItemMenuText>AJUDA</ItemMenuText>
+                </ItemButtonList>
             </DivMenuList>
         </Container>
     );
